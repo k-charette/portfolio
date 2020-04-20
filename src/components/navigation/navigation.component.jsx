f7baebeec58ecf39d18a72016c5f4248193bdac9
Ken Charette <charettekenneth@gmail.com>
2020-04-20 18:08:56 -0400

    Small styling change to the nav bar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -42,11 +42,11 @@ const Navigation = () => {
         <div>
           <nav className='md:pt-0 relative z-20 navbar-expand-lg'>
                 <div className='container my-2 mx-auto max-w-4xl md:flex text-xs sm:text-xs md:text-lg md:justify-start text-gray-100 uppercase font-medium'>
-                    <Link className='px-2 md:pl-0 md:mr-3 md:pr-3 hover:text-blue-400 focus:text-blue-400' smooth to='/#home-section'>Home</Link>
-                    <Link style={{color: aboutText}} className='px-2 md:pl-0 md:mr-3 md:pr-3 hover:text-blue-400 focus:text-blue-400'smooth to='/#about-section'>About</Link>
-                    <Link style={{color: projectText}} className='px-2 md:pl-0 md:mr-3 md:pr-3 hover:text-blue-400 focus:text-blue-400' smooth to='/#projects-section'>Projects</Link>
-                    <Link style={{color: techText}} className='px-2 md:pl-0 md:mr-3 md:pr-3 hover:text-blue-400 focus:text-blue-400' smooth to='/#technology-section'>Technology</Link>
-                    <a className='px-2 md:pl-0 md:mr-3 md:pr-3 hover:text-blue-400 focus:text-blue-400' href='https://pdfhost.io/v/P6T5WnsCh_KennethCharetteFullStackDeveloperResumepdf.pdf' rel="noopener noreferrer" target="_blank">Resume</a>
+                    <Link className='px-2 md:pl-0 md:mr-3 md:pr-3 focus:text-blue-400' smooth to='/#home-section'>Home</Link>
+                    <Link style={{color: aboutText}} className='px-2 md:pl-0 md:mr-3 md:pr-3 focus:text-blue-400'smooth to='/#about-section'>About</Link>
+                    <Link style={{color: projectText}} className='px-2 md:pl-0 md:mr-3 md:pr-3 focus:text-blue-400' smooth to='/#projects-section'>Projects</Link>
+                    <Link style={{color: techText}} className='px-2 md:pl-0 md:mr-3 md:pr-3 focus:text-blue-400' smooth to='/#technology-section'>Technology</Link>
+                    <a className='px-2 md:pl-0 md:mr-3 md:pr-3 focus:text-blue-400' href='https://pdfhost.io/v/P6T5WnsCh_KennethCharetteFullStackDeveloperResumepdf.pdf' rel="noopener noreferrer" target="_blank">Resume</a>
                 </div>
             </nav>
         </div>
