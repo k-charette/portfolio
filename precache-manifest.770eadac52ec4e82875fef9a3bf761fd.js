41ca77ee31996c9d3993a08b0a430e0a1744d3bd
Ken Charette <charettekenneth@gmail.com>
2020-05-09 11:08:50 -0400

    Updates

---
+++ precache-manifest.770eadac52ec4e82875fef9a3bf761fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8730fdc8b01b292d278ab6cb9fe78bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43d68704fe949baedb0d",
+    "url": "/static/css/main.25a810da.chunk.css"
+  },
+  {
+    "revision": "572203bbda9a410dd164",
+    "url": "/static/js/2.f544671a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f544671a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d68704fe949baedb0d",
+    "url": "/static/js/main.3079b1dc.chunk.js"
+  },
+  {
+    "revision": "92a8fa6d2a61009e0c1c",
+    "url": "/static/js/runtime-main.e3441cc1.js"
+  },
+  {
+    "revision": "30b6614ed301706fbf3551e1e965d204",
+    "url": "/static/media/abstract.30b6614e.jpg"
+  },
+  {
+    "revision": "3ab06f519266d97098280f33b84874ce",
+    "url": "/static/media/ken-profile.3ab06f51.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.89427b1886e1c792ae76716bcbbea22e.js"
+  "/precache-manifest.770eadac52ec4e82875fef9a3bf761fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
