11ca48e9bc97bb1173bd09b26a6e2a5a54d72a23
Ken Charette <charettekenneth@gmail.com>
2020-05-24 00:16:19 -0400

    Added more proejct icons

--- src/components/project-item/project.item.component.jsx
+++ src/components/project-item/project.item.component.jsx
@@ -4,14 +4,34 @@ import './project.item.styles.css'
 const ProjectItem = () => {
     return(
         <>
+          <div className='my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4'>
+                <div className='content shadow-2xl'>
+                    <div className='content-overlay'></div> 
+                     <img src='https://i.imgur.com/rtSWutm.png' alt='melange' className='w-full h-auto content-image'/>
+                     <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
+                        <h3 className='mb-8 font-semibold text-lg'>Weather Us</h3>
+                        <a href="https://weather-us.netlify.app/" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>LIVE DEMO</button>
+                        </a>
+                        <br />
+                        <a href="https://github.com/k-charette/weather-me" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 mt-4 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>CODE REPO</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
              <div className='my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4'>
                 <div className='content shadow-2xl'>
                     <div className='content-overlay'></div> 
-                     <img src='https://i.imgur.com/YxviJPh.png' alt='melange' className='w-full h-auto content-image'/>
+                     <img src='https://i.imgur.com/VRRZwh0.png' alt='melange' className='w-full h-auto content-image'/>
                      <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
                         <h3 className='mb-8 font-semibold text-lg'>Melange</h3>
                         <a href="https://melange-dune.herokuapp.com/" rel="noopener noreferrer" target="_blank">
-                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>VIEW SITE</button>
+                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>LIVE DEMO</button>
+                        </a>
+                        <br />
+                        <a href="https://github.com/k-charette/melange" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 mt-4 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>CODE REPO</button>
                         </a>
                     </div>
                 </div>
@@ -23,7 +43,11 @@ const ProjectItem = () => {
                      <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
                         <h3 className='mb-8 font-semibold text-lg'>Crown Shop</h3>
                         <a href="https://crwn-shopper.herokuapp.com/" rel="noopener noreferrer" target="_blank">
-                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>VIEW SITE</button>
+                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>LIVE DEMO</button>
+                        </a>
+                        <br />
+                        <a href="https://github.com/k-charette/crown-shop" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 mt-4 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>CODE REPO</button>
                         </a>
                     </div>
                 </div>
@@ -35,7 +59,11 @@ const ProjectItem = () => {
                     <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
                         <h3 className='mb-8 font-semibold text-lg'>Pathways</h3>
                         <a href="http://path-ways.herokuapp.com/" rel="noopener noreferrer" target="_blank">
-                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>VIEW SITE</button>
+                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>LIVE DEMO</button>
+                        </a>
+                        <br />
+                        <a href="https://github.com/k-charette/pathways" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 mt-4 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>CODE REPO</button>
                         </a>
                     </div>
                 </div>
@@ -47,7 +75,11 @@ const ProjectItem = () => {
                     <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
                         <h3 className='mb-8 font-semibold text-lg'>Fruits of the World</h3>
                         <a href="http://fruits-of-the-world.herokuapp.com/" rel="noopener noreferrer" target="_blank">
-                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>VIEW SITE</button>
+                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>LIVE DEMO</button>
+                        </a>
+                        <br />
+                        <a href="https://github.com/7KAG7/fruits_of_the_world" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 mt-4 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>CODE REPO</button>
                         </a>
                     </div>
                 </div>
--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
                     <div className='container max-w-4x1 mx-auto text-center pb-5'>
                         <h3 className='text-xl md:text-3xl leading-tight text-center max-w-md mx-auto text-gray-900 font-medium uppercase'>Check out my projects</h3>
                     </div>
-                    <div className='container max-w-4x1 mx-auto text-center flex flex-wrap items-start md:flex-no-wrap'>
+                    <div className='container max-w-4x1 mx-auto text-center flex flex-wrap items-start md:flex-wrap'>
                         <ProjectItem />
                     </div>
                 </div>
