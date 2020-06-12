ded1c3f92d1c049b563b1073266a7358b5c55b47
Ken Charette <charettekenneth@gmail.com>
2020-06-11 20:48:20 -0400

    Updating project links

--- src/components/project-item/project.item.component.jsx
+++ src/components/project-item/project.item.component.jsx
@@ -4,10 +4,26 @@ import './project.item.styles.css'
 const ProjectItem = () => {
     return(
         <>
+        <div className='my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4'>
+                <div className='content shadow-2xl'>
+                    <div className='content-overlay'></div> 
+                     <img src='https://i.imgur.com/70VrdcY.png' alt='civ' className='w-full h-auto content-image'/>
+                     <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
+                        <h3 className='mb-8 font-semibold text-lg'>Civilization Sim</h3>
+                        <a href="https://civ-sim.netlify.app/" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>LIVE DEMO</button>
+                        </a>
+                        <br />
+                        <a href="https://github.com/k-charette/mintbean-civ" rel="noopener noreferrer" target="_blank">
+                            <button className='border-2 border-blue-400 px-5 py-1 mt-4 tracking-widest font-medium hover:bg-blue-400 hover:text-white'>CODE REPO</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
           <div className='my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4'>
                 <div className='content shadow-2xl'>
                     <div className='content-overlay'></div> 
-                     <img src='https://i.imgur.com/rtSWutm.png' alt='melange' className='w-full h-auto content-image'/>
+                     <img src='https://i.imgur.com/rtSWutm.png' alt='weather' className='w-full h-auto content-image'/>
                      <div className='content-details fadeIn-top font-sans leading-relaxed text-white'>
                         <h3 className='mb-8 font-semibold text-lg'>Weather Us</h3>
                         <a href="https://weather-us.netlify.app/" rel="noopener noreferrer" target="_blank">
